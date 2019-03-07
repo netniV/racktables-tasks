21abc2e0833cf0e88be0563647ca4ecffe08474d
netniV <netniv@hotmail.com>
2019-03-07 12:38:40 +0000

    Update 'existsConfigVar' function

--- inc/compat.php
+++ inc/compat.php
@@ -2,13 +2,29 @@
 if (!function_exists('existsConfigVar')) {
 	function existsConfigVar ($varname)
 	{
-		$result = usePreparedSelectBlade
-		(
-			'SELECT COUNT(*) '.
-			'FROM Config C '.
-			'WHERE varname = ?',
-			array($varname)
-		);
-		return $result->fetch (PDO::FETCH_NUM);
+		global $configCache;
+		if (!isset ($configCache))
+			throw new RackTablesError ('configuration cache is unavailable', RackTablesError::INTERNAL);
+
+		if (!NULL === $value = array_fetch ($configCache, $varname, NULL))
+		{
+			error_log("existsConfigVar ($varname) [cache]: 1");
+			$value = 1;
+		}
+		else
+		{
+			$result = usePreparedSelectBlade
+			(
+				'SELECT COUNT(*) '.
+				'FROM Config C '.
+				'WHERE varname = ?',
+				array($varname)
+			);
+			$rows = $result->fetch (PDO::FETCH_NUM);
+			$value = count ($rows) == 1 ? $rows[0][0] : 'NULL';
+			error_log("existsConfigVar ($varname) [db]: $value");
+		}
+
+		return intval($value);
 	}
 }
